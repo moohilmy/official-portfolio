c578b6f51f88d929adb37242f564a6caa1269cf2
moohilmy <hilmy7132@gmail.com>
2025-02-10 21:34:14 +0200

    add delay to landing page

--- my-app/app/components/Landing/Landing.tsx
+++ my-app/app/components/Landing/Landing.tsx
@@ -4,7 +4,7 @@ import style from './style.module.css'
 const Landing = () => {
   return (
     <div className="h-[85vh] relative" >
-       <motion.div className={style.landingBoxGlass} initial={{ opacity: 0}} animate={{ opacity: 1}}>
+       <motion.div className={style.landingBoxGlass} initial={{ opacity: 0}} animate={{ opacity: 1 }} transition={{delay: .4}}>
         <h1 className={style.name}>Hallo, I am <span>Mohamed Helmy</span></h1>
         <p className={style.description}>A Front-end Developer passionate about delivering fast, responsive, and engaging digital experiences.</p>
       </motion.div> 
