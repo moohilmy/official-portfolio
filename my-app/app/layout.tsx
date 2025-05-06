3d1db4a2d4e115a8f010e99050224ea8d26a02ac
moohilmy <hilmy7132@gmail.com>
2025-05-06 13:54:00 +0300

    end Improve SEO

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -13,7 +13,7 @@ const SpaceGrotesk = Space_Grotesk({
 export const metadata: Metadata = {
   title: "HELMY DEV | PORTFOLIO ",
   description: "Explore the professional frontend portfolio of Helmy Dev — expert in React, TypeScript, Next.js, and modern web technologies.",
-  keywords: ["Frontend Developer", "React Developer", "Next.js Portfolio", "TypeScript Developer", "Web Developer Portfolio", 'egyptian developer', 'web developer', 'frontend developer', 'software engineer', 'web designer', 'مطور واجهات أمامية', 'مطور ويب', 'مطور تطبيقات ويب', 'مطور تطبيقات الويب', 'مطور تطبيقات الويب في مصر', 'مطور واجهات أمامية في مصر', 'مطور واجهات أمامية في القاهرة', 'مطور واجهات أمامية في الإسكندرية'],
+  keywords: ["Frontend Developer", 'Helmy Dev', '@moohilmy' , 'moohilmy', 'moohelmy', 'Hilmy dev', 'حلمي للتطوير', "React Developer", "Next.js Portfolio", "TypeScript Developer", "Web Developer Portfolio", 'egyptian developer', 'web developer', 'frontend developer', 'software engineer', 'web designer', 'مطور واجهات أمامية', 'مطور ويب', 'مطور تطبيقات ويب', 'مطور تطبيقات الويب', 'مطور تطبيقات الويب في مصر', 'مطور واجهات أمامية في مصر', 'مطور واجهات أمامية في القاهرة', 'مطور واجهات أمامية في الإسكندرية'],
   authors: [{ name: "Helmy Dev", url: "https://official-portfolio-ecru.vercel.app" }],
   creator: "Helmy Dev",
   themeColor: "#1e73be",
